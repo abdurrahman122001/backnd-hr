4b5840544741be82645764359a17696b8f763424
Abdur Rahman <qaziabdurrahman12@gmail.com>
2025-06-27 18:51:08 +0500

    updated code

--- src/index.js
+++ src/index.js
@@ -44,10 +44,7 @@ const sslOptions = {
   cert: fs.readFileSync("/root/server.crt"),
 };
 
-
-// Create HTTPS server
 const server = https.createServer(sslOptions, app);
-
 // Initialize Socket-IO
 const { Server } = require("socket.io");
 const io = new Server(server, { cors: { origin: "*" } });
